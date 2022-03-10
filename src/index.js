9a4946022e8a435abbaa55f587773155df906f54
Kiryl Yermalovich <ts.underawater@gmail.com>
2022-03-10 17:32:26 +0300

    add: added loader and result component

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import './global.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { GlobalContextProvider } from './context/Context';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <GlobalContextProvider>
+      <Router>
+        <App />
+      </Router>
+    </GlobalContextProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
